7d9e46c1886cf5bce442ac3c75603c02aefb3076
Venugopal K <venu.shastri@gmail.com>
2024-06-26 23:05:25 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,12 +10,3 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
-TEST(SoundexTest, HandlesSimpleCases) {
-    EXPECT_EQ(generateSoundex("Robert"), "R163");
-    EXPECT_EQ(generateSoundex("Rupert"), "R163");
-    EXPECT_EQ(generateSoundex("Rubin"), "R150");
-    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
-    EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
-    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
-    EXPECT_EQ(generateSoundex("Pfister"), "P236");
-}
