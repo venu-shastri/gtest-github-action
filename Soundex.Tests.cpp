96c0ccda60fc00c3f4378910b33af419839e9b98
Venugopal K <venu.shastri@gmail.com>
2024-06-26 22:59:08 +0530

    Initial commit

---
+++ Soundex.Tests.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "Soundex.h"
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
+  generateSoundex("AX", soundex);
+ // ASSERT_EQ(soundex,"A200");
+}
