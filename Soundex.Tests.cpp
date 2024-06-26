6239ef8bb6dcd14d0d0a891a6741b04a1158da92
Venugopal K <venu.shastri@gmail.com>
2024-06-26 23:02:52 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,21 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+
+TEST(SoundexTest, HandlesEmptyString) {
+    EXPECT_EQ(generateSoundex(""), "");
+}
+
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, HandlesSimpleCases) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
 }
