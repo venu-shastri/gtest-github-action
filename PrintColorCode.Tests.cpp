6c58acc129c715aa296c056fe379e4994e4546da
Venugopal K <venu.shastri@gmail.com>
2024-08-20 14:25:35 +0530

    Update PrintColorCode.Tests.cpp

--- PrintColorCode.Tests.cpp
+++ PrintColorCode.Tests.cpp
@@ -1,2 +1,13 @@
 #include "PrintColorCode.h"
+#include <gtest/gtest.h>
+
+TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCount){
+//Arrange
+  int expectedCount=25;
+ //Act
+  int actualCount=printColorCode();
+  //Assert
+  ASSERT_EQ(expectedCount,actualCount);
+  
+}
 
