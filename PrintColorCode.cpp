f1e5860e4e50c24cadcf06189b315fa5453e3d82
Venugopal K <venu.shastri@gmail.com>
2024-08-20 14:17:44 +0530

    Create PrintColorCode.cpp

---
+++ PrintColorCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <assert.h>
+
+int printColorMap() {
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+        }
+    }
+    return i * j;
+}
