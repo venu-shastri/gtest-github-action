f5d2a6e234d6e7764003cba06b642ebafd4428d1
Venugopal K <venu.shastri@gmail.com>
2024-08-20 15:01:32 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -7,7 +7,7 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_strin
   string input="";
   int expectedsum=0;
   //Act
-  int actualSum=calculator.add(input);
+  int actualSum=calculator.Add(input);
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
