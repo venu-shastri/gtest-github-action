ccd6dce06b6e42c8cca8a7a9461f844b6b7933b5
Venugopal K <venu.shastri@gmail.com>
2024-08-21 17:24:49 +0530

    Update TestChallengeUsingGmock.Tests.cpp

--- TestChallengeUsingGmock.Tests.cpp
+++ TestChallengeUsingGmock.Tests.cpp
@@ -1,1 +1,24 @@
+#include "TestChallengeUsingGmock.h"
+#include <gmock/gmock.h>
 
+using ::testing::Return;
+using ::testing::FloatEq;
+
+//Mock Class
+class NetworkAlerterMock:public INetworkAlerter{
+public:
+MOCK_METHOD(int, alert,(float celcius));
+MOCK_METHOD( int,  getFailureCount,());
+MOCK_METHOD( void ,incrementFailureCount,(int seed));
+MOCK_METHOD(void, resetFailureCount,());
+};
+
+
+TEST(AlertCelciusTestSuite,InteractionTesting){
+    NetworkAlerterMock mockObj;
+    //Expectation 
+      EXPECT_CALL(mockObj,alert(FloatEq(37.7778)).Times(1).WillRepeatedly(Return(500));
+    //Interaction
+    alertInCelcius(100.00004,&mockObj);
+  
+}
