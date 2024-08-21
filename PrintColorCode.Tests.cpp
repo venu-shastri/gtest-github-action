f9631c5fd2020283642f85749ab3cca1c0b1fda7
Venugopal K <venu.shastri@gmail.com>
2024-08-21 12:58:02 +0530

    Update PrintColorCode.Tests.cpp

--- PrintColorCode.Tests.cpp
+++ PrintColorCode.Tests.cpp
@@ -1,11 +1,14 @@
 #include "PrintColorCode.h"
 #include <gtest/gtest.h>
-
+//TestDouble, FakeDependnecy
+void FakePrinter(int colorCode, const char* majorColor,const char* minorColor){
+  
+}
 TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCount){
 //Arrange
   int expectedCount=25;
  //Act
-  int actualCount=printColorMap();
+  int actualCount=printColorMap(&FakePrinter);
   //Assert
   ASSERT_EQ(expectedCount,actualCount);
   
