5bf21b9865e7b5139066accdb5f1b4e37794a7c6
Venugopal K <venu.shastri@gmail.com>
2024-08-21 14:16:04 +0530

    Update PrintColorCode.h

--- PrintColorCode.h
+++ PrintColorCode.h
@@ -1,2 +1,11 @@
 
-int printColorMap(void(*printer)(int colorCode,const char* majorColor,const char* minorColor));
+//int printColorMap(void(*printer)(int colorCode,const char* majorColor,const char* minorColor));
+
+class IPrinter{
+public:
+      virtual ~IPrinter(){}
+      virtual void print(int colorCode,const char* majorColor,const char* minorColor)=0;
+};
+
+int printColorMap(IPrinter* printer);
+
