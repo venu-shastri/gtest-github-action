9fa2694db4f4d7ba8aa94c51443ebe93add20b32
Venugopal K <venu.shastri@gmail.com>
2024-08-21 16:57:18 +0530

    Create TestChallengeUsingGmock.cpp

---
+++ TestChallengeUsingGmock.cpp
@@ -0,0 +1,22 @@
+#include "TestChallengeUsingGmock.h"
+
+//RealTime Implementation
+class RealTimeNetowrkAleter :public INetworkAlerter{
+private : int failureCount=0;
+public:
+
+ int alert(float celcius){ return 200;}
+ int  getFailureCount(){ return failureCount;}
+ void icrementFailureCount(int seed) { failureCount+=seed;}
+  vod resetFailureCount(){ failureCount=0;}
+
+};
+
+void alertInCelcius(float farenheit,INetworkAlerter* networkAlerter){
+
+   float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlerter->alert(celcius);
+    if (returnCode != 200) {
+         icrementFailureCount(1);
+    }
+}
