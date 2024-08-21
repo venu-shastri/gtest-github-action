dbf5fc91cc846177a1ca899f3fbb44ea1d0bc691
Venugopal K <venu.shastri@gmail.com>
2024-08-21 12:53:50 +0530

    Update PrintColorCode.cpp

--- PrintColorCode.cpp
+++ PrintColorCode.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <assert.h>
 #include "PrintColorCode.h"
-int printColorMap() {
+
+void printOnTerminal(int colorCode,const char* majorColor,const char* mincorColor){
+    std::cout<<colorCode<<"|"<<majorColor<<"|"<<minorColor<<std::endl;
+}
+int printColorMap(void (*printer)(int colorCode,const char* majorColor,const char* mincorColor)) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            printer(i * 5 + j , majorColor[i], minorColor[i] );
+            //std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
