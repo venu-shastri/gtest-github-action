a072b79c87c85284baa24bb24dc55e0ca812e3ae
Venugopal K <venu.shastri@gmail.com>
2024-08-21 14:26:17 +0530

    Update PrintColorCode.cpp

--- PrintColorCode.cpp
+++ PrintColorCode.cpp
@@ -31,3 +31,17 @@ int printColorMap(IPrinter* printer){
     }
     return i * j;
 }
+
+//C++
+int printColorMap(std::function<void(int,const char*,const char*)> printer){
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            printer(i * 5 + j , majorColor[i], minorColor[i] );
+            //std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+        }
+    }
+    return i * j;
+}
