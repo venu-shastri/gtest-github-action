2733f936da66b5c371444d0afe97ccba54b3ad01
Venugopal K <venu.shastri@gmail.com>
2024-08-21 10:19:35 +0530

    Create DataDrivenTests.cpp

---
+++ DataDrivenTests.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include "StringCalculator.h"
+class string_calculator_add_Fixture:public testing::Test{
+  protected:
+  StringCalculator calculator;
+
+};
+class string_calculator_add_Parameter_Fixture:
+                                 public string_calculator_add_Fixture, 
+                                 public testing:: testing::WithParamInterface<std::string>{
+  protected:
+  StringCalculator calculator;
+
+};
+INSTANTIATE_TEST_SUITE_P(SingleNumerParameter,
+                         string_calculator_add_Parameter_Fixture,
+                         testing::Values("1"));
+
+TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_string){
+ //Arrange
+  StringCalculator calculator;
+  string input=;
+  int expectedsum=0;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST_F(string_calculator_add_Fixture,when_passed_a_single_number_returns_0_for_zero){
+ //Arrange
+  string input="0";
+  int expectedsum=0;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST_P(string_calculator_add_Parameter_Fixture,when_passed_a_single_number_returns_1_for_one){
+ //Arrange
+  StringCalculator calculator;
+  string input=GetParam();
+  int expectedsum=1;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_two_comma_delimited_numbers,returns_their_sum){
+ //Arrange
+  StringCalculator calculator;
+  string input="1,2";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
