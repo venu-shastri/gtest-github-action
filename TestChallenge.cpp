fdba31d00f84cbb55040ac4c77d7d4d49fc7ada6
Venugopal K <venu.shastri@gmail.com>
2024-08-21 16:00:06 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -7,13 +7,13 @@ int realTimeNetworkAlerter(float celcius) {
    //Real Network Interaction Happens Here
     // Return 200 for ok
     // Return 500 for not-ok
-  if(celcius % 2==0){
-    return 200;
-  }
+  //if(celcius % 2==0){
+    //return 200;
+  //}
   return 500;
 }
 
-void void alertInCelcius(float farenheit,int(*networkAlerter)(float celcius)); {
+ void alertInCelcius(float farenheit,int(*networkAlerter)(float celcius)); {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlerter(celcius);
     if (returnCode != 200) {
