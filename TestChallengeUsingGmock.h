fea381fb93273f366ae13a0de98dd967ea9f0718
Venugopal K <venu.shastri@gmail.com>
2024-08-21 16:58:47 +0530

    Update TestChallengeUsingGmock.h

--- TestChallengeUsingGmock.h
+++ TestChallengeUsingGmock.h
@@ -4,6 +4,8 @@ public:
 vitual ~INetworkAlerter(){}
 virtual int alert(float celcius)=0;
 virtual int  getFailureCount()=0;
+virtual void icrementFailureCount(int seed)=0;
+virtual vod resetFailureCount()=0;
 };
 
 void alertInCelcius(float farenheit,INetworkAlerter* networkAlerter);
