79e698b83b6ae1a09106113fa71164df692c0502
Venugopal K <venu.shastri@gmail.com>
2024-08-21 09:53:19 +0530

    Update StringCalculatorFixture.Tests.cpp

--- StringCalculatorFixture.Tests.cpp
+++ StringCalculatorFixture.Tests.cpp
@@ -5,7 +5,7 @@
 class StringCalculatorAddTestFixture:public testing::Test{
   protected:
     //Common Reusable Code
-    StringCalculator calculator;
+    StringCalculator *calculator;
     //Hook Callbacks
   // You can define per-test-suite set-up logic as usual.
      static void SetUpTestSuite() {
@@ -17,11 +17,13 @@ class StringCalculatorAddTestFixture:public testing::Test{
      }
   // You can define per-test set-up logic as usual.
   void SetUp() override {
+    calculator=new StringCalculator();
     std::cout<<"Test Setup"<<std::endl;
   }
 
   // You can define per-test tear-down logic as usual.
   void TearDown() override {
+    delete calculator;
     std::cout<<"Test Teardown"<<std::endl;
   }
 
@@ -32,7 +34,7 @@ TEST_F(StringCalculatorAddTestFixture,when_passed_a_single_number_returns_0_for_
   string input="";
   int expectedsum=0;
   //Act
-  int actualSum=calculator.Add(input);
+  int actualSum=calculator->Add(input);
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
@@ -41,7 +43,7 @@ TEST_F(StringCalculatorAddTestFixture,when_passed_a_single_number_returns_0_for_
   string input="0";
   int expectedsum=0;
   //Act
-  int actualSum=calculator.Add(input);
+  int actualSum=calculator->Add(input);
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
