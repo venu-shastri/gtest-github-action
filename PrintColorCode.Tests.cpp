e31c0adbdf2d51719c65181f9fe6355625b4cc93
Venugopal K <venu.shastri@gmail.com>
2024-08-21 14:19:42 +0530

    Update PrintColorCode.Tests.cpp

--- PrintColorCode.Tests.cpp
+++ PrintColorCode.Tests.cpp
@@ -3,13 +3,30 @@
 
 //TestDouble, FakeDependnecy, Stub , Dumb
 void FakePrinter(int colorCode, const char* majorColor,const char* minorColor){
-  
+
 }
+
+class FakePrinterTestDouble:public IPrinter{
+
+public:
+      void print(int colorCode, const char* majorColor,const char* minorColor){
+      }
+};
 TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCount){
 //Arrange
   int expectedCount=25;
  //Act
   int actualCount=printColorMap(&FakePrinter);
+  //Assert
+  ASSERT_EQ(expectedCount,actualCount);
+  
+}
+TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCountUsingStub){
+//Arrange
+  int expectedCount=25;
+  FakePrinterTestDouble dependency;
+ //Act
+  int actualCount=printColorMap(&dependency);
   //Assert
   ASSERT_EQ(expectedCount,actualCount);
   
